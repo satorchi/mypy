0ff9b98eedebd3a706416ff954bf9df7ad1e1ec9
satorchi <satorchi@apc.in2p3.fr>
2018-12-22 23:00:47 +0100

    read various ISO date format

--- satorchipy/datefunctions.py
+++ satorchipy/datefunctions.py
@@ -76,13 +76,19 @@ def str2dt(datestr):
     datestr=datestr.replace(',','.')
     
     fmts=["%Y-%m-%d %H:%M:%S.%f",
-          "%Y%m%d-%H%M%S.%f",
           "%Y-%m-%d %H:%M:%S",
           "%Y-%m-%d %H:%M:%SZ",
           "%Y-%m-%d %H:%M:%S.%fZ",
           '%Y-%m-%d %H:%M:%S.%f UT',
           '%Y-%m-%d %H:%M:%S UT',
           "%Y-%m-%d %H:%M",
+          "%Y-%m-%dT%H:%M:%S.%f",
+          "%Y-%m-%dT%H:%M:%S",
+          "%Y-%m-%dT%H:%M:%SZ",
+          "%Y-%m-%dT%H:%M:%S.%fZ",
+          '%Y-%m-%dT%H:%M:%S.%f UT',
+          '%Y-%m-%dT%H:%M:%S UT',
+          "%Y-%m-%dT%H:%M",          
           "%Y-%m-%d",
           "%Y%m%d-%H%M",
           "D%Y%m%dT%H%M%S",
@@ -90,7 +96,8 @@ def str2dt(datestr):
           "%b %d %H:%M:%S  %Y",
           "%d.%m.%Y  %H:%M",
           "%Y%m%dT%H%M%S",
-          "%Y%m%dT%H%M%S.%f"]
+          "%Y%m%dT%H%M%S.%f",
+          "%Y%m%d-%H%M%S.%f"]
 
 
     for fmt in fmts:
