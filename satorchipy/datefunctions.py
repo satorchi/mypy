d7afc0738d9a0080bd7765479a6a2e44c0878c64
Steve TORCHINSKY <satorchi@apc.in2p3.fr>
2020-09-21 15:29:21 +0200

    French September

--- satorchipy/datefunctions.py
+++ satorchipy/datefunctions.py
@@ -66,6 +66,7 @@ def str2dt(datestr):
     datestr=re.sub('[nN]ov\.','11',datestr)
     datestr=re.sub('[oO]ct\.','10',datestr)
     datestr=re.sub('[sS]ep\.','9',datestr)
+    datestr=re.sub('[sS]ept\.','9',datestr)
     datestr=re.sub('[aA]o.t\.','8',datestr)
     datestr=re.sub('[aA]o.t','8',datestr)
     datestr=re.sub('[aA]o.\.','8',datestr)
